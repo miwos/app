931cb9544adc21215dc0be7732e66a8d477c5622
arnoson <omasocke@hotmail.de>
2021-06-24 16:32:04 +0200

    Cleanup

--- src/components/Module.vue
+++ src/components/Module.vue
@@ -22,15 +22,7 @@
 <script setup lang="ts">
 import { computed, defineEmit, defineProps, ref, watch } from 'vue'
 import { useDragElement } from '../composables/useDragElement'
-import { useModules } from '../store/modules'
 import ConnectionPoint from './ConnectionPoint.vue'
-
-// TODO import interface from modules store
-interface ModuleDefinition {
-  type: string
-  inputs: number
-  outputs: number
-}
 
 const props = defineProps<{
   id: number
@@ -43,16 +35,12 @@ const emit = defineEmit(['update:position'])
 
 const el = ref<HTMLElement | null>(null)
 const { position } = useDragElement(el)
-// const modules = useModules()
-
 watch(position, (position) => emit('update:position', position))
 
 const style = computed(() => ({
   top: `${props.position.y}px`,
   left: `${props.position.x}px`,
 }))
-
-// const definition = modules.definitions[props.type]
 </script>
 
 <style scoped>
