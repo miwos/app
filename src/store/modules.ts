4a54919740e8048c5fa36de4be4781520e7adf29
arnoson <omasocke@hotmail.de>
2021-06-24 20:16:02 +0200

    Load module definitions from files

--- src/store/modules.ts
+++ src/store/modules.ts
@@ -1,10 +1,18 @@
 import { defineStore } from 'pinia'
 
+const definitionModules = import.meta.globEager('../modules/*.json')
+const definitions: Record<string, ModuleDefinition> = Object.fromEntries(
+  Object.values(definitionModules).map((module) => [
+    module.default.type,
+    module.default,
+  ])
+)
+
 export const useModules = defineStore({
   id: 'modules',
 
   state: () => ({
-    definitions: {} as Record<string, ModuleDefinition>,
+    definitions,
     items: {} as Record<number, Module>,
     nextModuleId: 0,
   }),
@@ -18,7 +26,7 @@ export const useModules = defineStore({
       if (!this.definitions[type])
         throw new Error(`Can't find module definition for type '${type}'.`)
       const id = this.nextModuleId++
-      this.items[id] = { id, type, position }
+      this.items[id] = { id, type, position, inputDeltas: [], outputDeltas: [] }
     },
 
     clearAll() {
