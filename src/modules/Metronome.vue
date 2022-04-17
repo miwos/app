890391a6d910be56976fbf1bf81f9099bcecbdc0
arnoson <omasocke@hotmail.de>
2022-04-17 17:46:57 +0200

    Disable metronome component for now

--- src/modules/Metronome.vue
+++ src/modules/Metronome.vue
@@ -1,5 +1,34 @@
-<template>
+<!-- <template>
   <svg class="pie" viewbox="0 0 100 100">
-    <circle cx="50%" cy="50%" r="25%" />
+    <circle
+      cx="50%"
+      cy="50%"
+      r="25%"
+      fill="red"
+      stroke-width="50%"
+      :stroke-dasharray="dashArray"
+    />
   </svg>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  time: number
+}>()
+
+const circumference = 70.6858
+
+const dashArray = computed(
+  () => `${circumference * (props.time / 1000)} ${circumference}`
+)
+</script>
+
+<style scoped>
+.pie {
+  width: 100%;
+  height: 100%;
+  stroke: blue;
+}
+</style> -->
