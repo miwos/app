525104d8caea2975f7b3fd974ab2ac7fbe01e8d9
arnoson <omasocke@hotmail.de>
2022-09-16 18:11:23 +0200

    feat: load local modules in dev mode

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ import './main.scss'
 
 const app = createApp(App)
 
+// I tried to avoid a monorepo, but still wanted to get HMR for miwos modules
+// development. So as a dirty workaround we're assuming the modules repo is
+// in the same parent folder.
+if (import.meta.env.DEV) {
+  // @ts-ignore
+  const modules = await import('../../modules/src/index.ts')
+  app.use(modules.default)
+}
+
 app.use(createPinia())
 app.use(router)
 
