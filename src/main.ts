29163a43f0a37b9e57cd309960b62bb1251452d8
arnoson <hallo@arno-schlipf.info>
2023-02-08 16:07:53 +0100

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import Previewer from 'virtual:vue-component-preview'
 
 import './main.scss'
 
@@ -20,6 +19,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(Previewer)
 
 app.mount('#app')
