c7699a588a26931fa5dde229404b9fba632d9311
arnoson <hallo@arno-schlipf.info>
2023-02-08 16:08:48 +0100

    chore: cleanup

--- src/modules/Strings.vue
+++ src/modules/Strings.vue
@@ -46,26 +46,3 @@ code {
   color: #304455;
 }
 </style>
-
-<preview lang="md">
-# This is preview page of HelloWorld.vue
-
-## Props
-
-| Props | Description   |
-| ----- | ------------- |
-| msg   | Title message |
-
-## Examples
-
-<script setup>
-const msgs = [
-  'Hello Peter',
-  'Hello John',
-];
-</script>
-
-<template v-for="msg in msgs">
-  <slot :msg="msg"></slot>
-</template>
-</preview>
