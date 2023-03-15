b80a13807b1078ccddc02f74dbc0e15ae76c881d
arnoson <hallo@arno-schlipf.info>
2023-03-15 18:40:12 +0100

    feat: register module props listeners

--- src/components/ModuleContent.vue
+++ src/components/ModuleContent.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="module-content" :style="{ clipPath: mask }">
-    <component :is="modules.get(props.module.type)" v-bind="module.props" />
+  <div class="module-content" :style="clipStyle">
+    <component
+      :is="moduleContents.get(props.module.type)"
+      v-bind="module.props"
+      v-on="updatePropHandlers"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useModuleDefinitions } from '@/stores/moduleDefinitions'
+import { useModules } from '@/stores/modules'
 import type { Module } from '@/types'
-import { defineAsyncComponent } from 'vue'
+import { computed, defineAsyncComponent } from 'vue'
 
 const props = defineProps<{ module: Module; mask: string }>()
+const definition = useModuleDefinitions().get(props.module.type)
+const clipStyle = computed(() =>
+  definition?.clipContent ?? true ? { clipPath: props.mask } : {}
+)
 
-const modulesImport = import.meta.glob('../modules/content/*.vue')
-const modules = new Map(
-  Object.entries(modulesImport).map(([path, asyncModule]) => {
+const moduleContentsImport = import.meta.glob('../modules/content/*.vue')
+const moduleContents = new Map(
+  Object.entries(moduleContentsImport).map(([path, asyncModule]) => {
     const name = path.match(/\/([\w_-]+).vue$/)![1]
     return [name, defineAsyncComponent(asyncModule as any)]
   })
+)
+
+const modules = useModules()
+const updatePropHandlers = computed(() =>
+  Object.fromEntries(
+    Object.keys(props.module.props).map((name) => [
+      `update:${name}`,
+      (value: any) => modules.updateProp(props.module.id, name, value),
+    ])
+  )
 )
 </script>
 
